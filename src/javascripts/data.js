2a696aeba3e5ec4185c75216b31aef35e8056947
velf <orsolya.vasarhelyi@gmail.com>
2017-09-24 16:32:39 +0200

    changing wording adding new position

--- src/javascripts/data.js
+++ src/javascripts/data.js
@@ -1,41 +1,49 @@
 let data = {
   user: {
     name: 'Orsolya Vásárhelyi',
-    role: ['networks', 'data science', 'product development'],
+    role: ['network & data scien', 'virality expert', 'your data consultant'],
     email: 'orsolya.vasarhelyi@gmail.com',
     github: 'https://github.com/velf',
     linkedin: 'https://www.linkedin.com/in/orsolya-vasarhelyi-43132925',
     twitter: 'https://twitter.com/Orsi_Vasarhelyi',
+    blog: 'https://medium.com/@velf',
     phrase: 'Connecting academia with the industry'
   },
   industry: [
+  {
+      date: '2017 / present',
+      title: 'Data Scientist Intern ',
+      company: 'Typeform',
+      companyUrl: 'https://www.typeform.com/',
+      description: "As a a network scientist my main task is to build a prediction model for product adoption through virality"
+    },
     {
       date: '2015 / present',
-      title: 'Freelance Data Analyst',
+      title: 'Freelance Data Scientist',
       company: '',
       companyUrl: '',
-      description: "Taking part in various #dataanalysis projects from e­health to artificial bots' behaviour monitoring #nlp #big data #market research #python #sql"
+      description: "Taking part in various data analysis projects; infleuncer identification, social media analysis"
     }, {
       date: '2014 / 2015',
       title: 'Head of Sales',
       company: 'Neticle Technologies',
       companyUrl: 'https://neticletech.com/',
-      description: "Understanding clients' media monitoring needs and implement their feedback into #software development. Building a #sales team from scratch. #nlp #product"
+      description: "Understanding clients' media monitoring needs and implement their feedback into software development. Building a sales team from scratch."
     }, {
       date: '2010 / 2014',
       title: 'Product Owner',
       company: 'Maven Seven Network',
       companyUrl: 'http://maven7.com/',
-      description: "Started as a #network scientist intern, became a full­time analyst then the product owner of the #social media analytical products. Position required beyond #advanced statistical analysis to take part in #project management and customer relations as well."
+      description: "Started as a network scientist intern, became a full­time analyst then the product owner of the #social media analytical products. Position required beyond advanced statistical analysis to take part in project management and customer relations as well."
     }
   ],
   academics: [
     {
       date: '2015 / present',
-      title: 'Phd Candidate in Network Science',
-      company: 'CNS, Central European University',
+      title: 'PhD Candidate in Network Science',
+      company: 'Center for Network Science, Central European University',
       companyUrl: 'https://cns.ceu.edu/',
-      description: "My research question is what is the difference between successful female and male career paths in project­ based environments. With a #temporal social network approach I analyse individual #success through collaborative #networks on data taken from #GitHub. Supervisor: Prof. Balázs Vedres Teacher Assistant of Prof. Albert­László Barabási"
+      description: "My research question is what is the difference between successful female and male career paths in project­ based environments. With a social network approach we analyse individual success through collaborative networks on data taken from GitHub. \n Supervisor: Prof. Balázs Vedres Teacher Assistant of Prof. Albert­László Barabási"
     }, {
       date: '2016 / present',
       title: 'Programming Lecturer',
@@ -70,17 +78,13 @@ let data = {
   ],
   activism: [
     {
-      title: 'Django girls',
+      title: 'Django Girls Budapest',
       url: 'https://djangogirls.org/budapest/',
       image: '/images/django.jpg'
     }, {
-      title: 'Bridge',
+      title: 'Bridge Budapest',
       url: 'http://bridgebudapest.org/',
       image: '/images/bridge.jpg'
-    }, {
-      title: 'DKonnekt',
-      url: 'https://www.facebook.com/konnektmentorprogram/',
-      image: '/images/konnet.jpg'
     }
   ]
 }
